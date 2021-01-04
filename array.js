908d0bfff993ab321d4a657fc9b8f4227c400865
neethu271 <neethuhoney5@gmail.com>
2021-01-04 15:52:59 +0530

    added forms

---
+++ array.js
@@ -0,0 +1,27 @@
+var arr=[100,200,300,400,500];
+
+console.log(arr);
+
+console.log(arr[0]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+
+
+
+var user={
+    username:"neethika",
+    password:"neethu123",
+    emailId:"neethu@gmail.com",
+
+}
+
+console.log(user);
+ 
+var user={
+    emailId:"neethu@gmail.com",
+    password:"neethu123",
+    username:"neethika",
+}
+
+console.log(user);
---
+++ js.html/global.js
@@ -0,0 +1,11 @@
+var a=100;
+var h=100;
+
+
+function f1(){
+
+          console.log("the value of a in f1 is "+a);
+
+}
+
+function h1()
